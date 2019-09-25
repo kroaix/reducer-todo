697f433be563c5b39f6cc556ef9aaafa984795ce
Katrina Roaix <kroaix@gmail.com>
2019-09-25 17:26:22 -0400

    MVP Complete

--- reducer-todo/src/components/TodoList.js
+++ reducer-todo/src/components/TodoList.js
@@ -2,15 +2,21 @@ import React from "react";
 import Todo from "./Todo";
 
 const TodoList = ({state, dispatch}) => {
-  return (
-    <div align="center">
-      {
-      state.map(todo =>
+  
+  const emptyTodo = state.length ? (
+    state.map(todo => {
+      return (
         <Todo todo={todo} key={todo.id} dispatch={dispatch} />
-      )
-    }
+      );
+    })
+  ) : (
+    <div className="empty">
+      <p>You have nothing to do!</p>
     </div>
+    
   );
+  
+  return <div align="center">{emptyTodo}</div>;
 };
 
 export default TodoList;
